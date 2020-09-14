150133325b0d8b1b94743948b50ff8421a216e4e
ravkishu <39808656+ravkishu@users.noreply.github.com>
2020-09-15 03:49:43 +0530

    Update smuggler.sh

--- arsenal/smuggler.sh
+++ arsenal/smuggler.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-dir=/root/Recon/$1
-
+dir=/root/workspace/$1
 mkdir $dir
 
-echo https://$1 | python3 /root/Tools/smuggler/smuggler.py > $dir/$1_smuggler
+# echo https://$1 | python3 /root/Tools/smuggler/smuggler.py > $dir/$1_smuggler
+echo $1 | httpx | python3 ~/tools/smuggler/smuggler.py | tee $dir/$1_smuggler
