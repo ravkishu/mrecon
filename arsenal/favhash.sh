19554570e2a675cb715b081cfd8b46745f7c6a18
ravkishu <39808656+ravkishu@users.noreply.github.com>
2020-09-15 16:08:07 +0530

    Update favhash.sh

--- arsenal/favhash.sh
+++ arsenal/favhash.sh
@@ -2,8 +2,7 @@
 
 dir=/root/workspace/$1
 
-mkdir $dir
 
-echo $1 | httpx | python3 ~/tools/FavFreak/favfreak.py > $dir/$1_faver
+echo $1 | httprobe --prefer-https | python3 ~/tools/FavFreak/favfreak.py > $dir/$1_faver
 
 cat $dir/$1_faver | grep 'h]' | cut -d ] -f2 | cut -d " " -f2 | tee $dir/$1_favhash_domain
