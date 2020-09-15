1613e43b67fe7024584803bab36d9bbd9683a303
ravkishu <39808656+ravkishu@users.noreply.github.com>
2020-09-15 11:12:59 +0530

    Update nuclear.sh

--- arsenal/nuclear.sh
+++ arsenal/nuclear.sh
@@ -3,4 +3,4 @@
 dir=/root/workspace/$1
 
 # cat $dir/$1_subdomains | httpx -silent | sort -u | nuclei -c 200 -silent -t /root/Tools/nuclei-templates/ -o $dir/$1_nuclei
-cat $dir/$1_subdomains | httpx -silent | sort -u | nuclei -silent -t ~/nuclei-templates/ -o $dir/$1_nuclei
+cat $dir/$1_subdomains | httpx -silent | sort -u | nuclei -silent -t ~/nuclei-templates/ | tee $dir/$1_nuclei
