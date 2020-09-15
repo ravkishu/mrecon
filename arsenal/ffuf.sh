e0aea7c4fee25c1b479d9fc6db1e2fd286ddd6fe
ravkishu <39808656+ravkishu@users.noreply.github.com>
2020-09-15 11:24:12 +0530

    Update ffuf.sh

--- arsenal/ffuf.sh
+++ arsenal/ffuf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dir=/root/Recon/$1
+dir=/root/workspace/$1
 
 
 ffuf -mc all -c -H "X-Forwarded-For: 127.0.0.1" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0" -u "$1/FUZZ" -w /root/wordlist/dicc.txt -D -e js,php,bak,txt,asp,aspx,jsp,html,zip,jar,sql,json,old,gz,shtml,log,swp,yaml,yml,config,save,rsa,ppk -ac -o $dir/$1.tmp
