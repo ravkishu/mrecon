63ba85e8994b6a29ec37ec91779d7c6534d1b1c0
ravkishu <39808656+ravkishu@users.noreply.github.com>
2020-09-15 16:06:58 +0530

    Update smuggler.sh

--- arsenal/smuggler.sh
+++ arsenal/smuggler.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 dir=/root/workspace/$1
-mkdir $dir
 
 # echo https://$1 | python3 /root/Tools/smuggler/smuggler.py > $dir/$1_smuggler
-echo $1 | httpx | python3 ~/tools/smuggler/smuggler.py | tee $dir/$1_smuggler
+echo $1 | httprobe --prefer-https | python3 ~/tools/smuggler/smuggler.py | tee $dir/$1_smuggler
 
 echo "Also Do manual testing via this command: 'python3 ~/tools/Smuggler/smuggler.py'"
